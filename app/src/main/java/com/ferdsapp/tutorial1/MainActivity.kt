69704dcc5ae275a04776ea8c99b93d0f42e11bd3
Ferdy Rahmaesa <ferdy.suarial@indocybere.id>
2024-07-03 11:41:06 +0700

    Slot Based Layout

--- app/src/main/java/com/ferdsapp/tutorial1/MainActivity.kt
+++ app/src/main/java/com/ferdsapp/tutorial1/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,6 +23,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -29,7 +35,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ferdsapp.tutorial1.ui.theme.Tutorial1Theme
@@ -110,7 +118,9 @@ private fun newTextPreview() {
 @Composable
 fun UserChat(name: String, msg: String) {
     Row(
-        modifier = Modifier.padding(4.dp)
+        modifier = Modifier
+            .padding(4.dp)
+            .clickable(onClick = {})
     ) {
         Image(painter = painterResource(
             id = R.drawable.ic_launcher_foreground),
@@ -125,8 +135,8 @@ fun UserChat(name: String, msg: String) {
         Column(modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()) {
-            Text(text = name)
-            Text(text = msg)
+            Text(text = name, fontWeight = FontWeight.Bold)
+            Text(text = msg, modifier = Modifier.offset(x = 4.dp))
             Icon(imageVector = Icons.Default.CheckCircle, contentDescription = null, modifier =
             Modifier.align(Alignment.End))
         }
@@ -138,5 +148,29 @@ fun UserChat(name: String, msg: String) {
 private fun UserChatPreview() {
     Tutorial1Theme {
         UserChat(name = "Freds", msg = "Android Dev")
+    }
+}
+
+@Composable
+fun SlotBasedLayout() {
+    Button(
+        contentPadding = PaddingValues(start = 20.dp, end = 20.dp, bottom = 12.dp, top = 12.dp),
+        onClick = {/*TODO*/ }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Favorite ,
+            contentDescription = null,
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text("Like")
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+private fun SlotBasedLayoutPreview() {
+    Tutorial1Theme {
+        SlotBasedLayout()
     }
 }
