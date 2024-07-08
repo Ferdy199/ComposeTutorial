11b7f9404fb6db52163283f244a2963c3450940d
Ferdy Rahmaesa <ferdy.suarial@indocybere.id>
2024-07-08 16:45:17 +0700

    finish topBar

--- app/src/main/java/com/gunder/market/component/MainTopBar.kt
+++ app/src/main/java/com/gunder/market/component/MainTopBar.kt
@@ -1,12 +1,18 @@
 package com.gunder.market.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Search
@@ -18,22 +24,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gunder.market.ui.theme.MarketTheme
+import kotlin.math.min
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopBar(modifier: Modifier = Modifier) {
-    Column {
+    Column(modifier = Modifier.padding(16.dp)) {
         Row(
-            modifier = Modifier.width(IntrinsicSize.Max),
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier.width(IntrinsicSize.Min),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedTextField(
                 value = "",
                 onValueChange = {},
-                modifier = modifier,
+                modifier = modifier.widthIn(min = 220.dp),
                 leadingIcon = {
                     Icon(
                         imageVector = Icons.Outlined.Search,
@@ -63,6 +73,25 @@ fun MainTopBar(modifier: Modifier = Modifier) {
                 imageVector = Icons.Outlined.Menu,
                 contentDescription = null,
                 modifier = modifier.size(24.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+            .padding(top = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.LocationOn,
+                contentDescription = null,
+                modifier = modifier.size(18.dp)
+            )
+            Text(text = "Dikirim Ke Alamat", fontSize = 12.sp)
+            Text(text = "Ferdy Rahmaesa", fontSize = 12.sp, fontWeight = FontWeight.Bold)
+            Icon(
+                imageVector = Icons.Outlined.KeyboardArrowDown,
+                contentDescription = null,
+                modifier = modifier.size(18.dp)
             )
         }
     }
