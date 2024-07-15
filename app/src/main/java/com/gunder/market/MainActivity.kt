66e6d54dd79ccf5f8f502480e19cab52afba001e
Ferdy Rahmaesa <ferdy.suarial@indocybere.id>
2024-07-15 16:22:32 +0700

    add top menu finished

--- app/src/main/java/com/gunder/market/MainActivity.kt
+++ app/src/main/java/com/gunder/market/MainActivity.kt
@@ -3,8 +3,13 @@ package com.gunder.market
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -13,7 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gunder.market.component.MainTopBar
+import com.gunder.market.component.TopMenu
+import com.gunder.market.model.dummyListTopMenu
 import com.gunder.market.ui.theme.MarketTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,8 +49,25 @@ fun MarketApp(modifier: Modifier = Modifier) {
     ) {
 //        your code compose here
         MainTopBar()
+        MainTopMenu()
     }
+}
 
+@Composable
+fun MainTopMenu(modifier: Modifier = Modifier) {
+    LazyRow{
+       items(dummyListTopMenu){
+            TopMenu(listTopMenu = it)
+       }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainTopMenuPreview() {
+    MarketTheme {
+        MainTopMenu()
+    }
 }
 
 @Preview(device = Devices.DEFAULT, showBackground = true)
